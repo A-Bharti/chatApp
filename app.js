dd7c0c3462d4a73828c27e0034ef466ba8df1aee
A-Bharti <68997912+A-Bharti@users.noreply.github.com>
2020-07-30 18:11:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+const PORT=3000;
 app.get('/', function(req, res) {
    res.sendfile('index.html');
 });
@@ -25,6 +26,6 @@ io.on('connection', function(socket) {
   
 
 
-http.listen(3000, function() {
+http.listen(PORT, function() {
    console.log('listening on *:3000');
 });
