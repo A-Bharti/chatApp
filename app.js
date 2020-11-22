9681ffd3a18b1db7faf879a72e58f839c9960435
A-Bharti <68997912+A-Bharti@users.noreply.github.com>
2020-11-22 22:58:39 +0530

    UI Changed

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const { count } = require('console');
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+ID=0;
 app.get('/', function(req, res) {
    res.sendfile('index.html');
 });
@@ -10,21 +13,38 @@ app.get('/', function(req, res) {
 // var nsp=io.of('/myid')
 io.on('connection', function(socket) {
 
-      socket.broadcast.emit('alert','someone connected now');
+      // socket.broadcast.emit('alert','someone connected now');
       socket.on('room',(room)=>{
          socket.join(room);
+         socket.room=room;
       })
     
 
    socket.on('sms',(sms)=>{
       console.log(sms)
+      
       io.sockets.in(sms.room).emit('sms',sms);
    })
+   socket.on('name',(name)=>{
+      socket.name=name;
+      var temp=''+ID;
+      socket.id=temp;
+      // console.log(typeof(temp),temp);
+      data={name:name,id:socket.id};
+      console.log(data);
+      io.sockets.in(socket.room).emit('name',data);
+      ID++;
+   })
+   socket.on('disconnect', function () {
+      data={name:socket.name,id:socket.id};
+      io.sockets.in(socket.room).emit('removed',data);
+    
+   });
   
 });
   
 
 
-http.listen(process.env.PORT, function() {
+http.listen(process.env.PORT  || 3000, function() {
    console.log('listening');
 });
