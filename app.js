27302505b50689507d0cc516a0fe73d9f14e12a8
A-Bharti <68997912+A-Bharti@users.noreply.github.com>
2020-11-23 09:00:36 +0530

    online user_list added

--- app.js
+++ app.js
@@ -4,47 +4,56 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-ID=0;
-app.get('/', function(req, res) {
+ID = 0;
+list = [];
+app.get('/', function (req, res) {
    res.sendfile('index.html');
 });
 
 //Whenever someone connects this gets executed
 // var nsp=io.of('/myid')
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
 
-      // socket.broadcast.emit('alert','someone connected now');
-      socket.on('room',(room)=>{
-         socket.join(room);
-         socket.room=room;
-      })
-    
-
-   socket.on('sms',(sms)=>{
-      console.log(sms)
-      
-      io.sockets.in(sms.room).emit('sms',sms);
+   // socket.broadcast.emit('alert','someone connected now');
+   socket.on('room', (room) => {
+      socket.join(room);
+      socket.room = room;
    })
-   socket.on('name',(name)=>{
-      socket.name=name;
-      var temp=''+ID;
-      socket.id=temp;
+
+
+   socket.on('sms', (sms) => {
+      console.log(sms)
+
+      io.sockets.in(sms.room).emit('sms', sms);
+   })
+   socket.on('name', (name) => {
+      socket.name = name;
+      var temp = '' + ID;
+      socket.id = temp;
       // console.log(typeof(temp),temp);
-      data={name:name,id:socket.id};
+      data = { name: name, id: socket.id };
+      list.push(data);
+      socket.emit('list', { list: list });
       console.log(data);
-      io.sockets.in(socket.room).emit('name',data);
+      io.sockets.in(socket.room).emit('name', data);
       ID++;
    })
    socket.on('disconnect', function () {
-      data={name:socket.name,id:socket.id};
-      io.sockets.in(socket.room).emit('removed',data);
-    
+      data = { name: socket.name, id: socket.id };
+      io.sockets.in(socket.room).emit('removed', data);
+      list.forEach(element => {
+         _id = element.id;
+         _name = element.name;
+         if (_id == socket.id) {
+            element.id = "NA";
+         }
+      });
    });
-  
+
 });
-  
 
 
-http.listen(process.env.PORT  || 3000, function() {
+
+http.listen(process.env.PORT || 3000, function () {
    console.log('listening');
 });
